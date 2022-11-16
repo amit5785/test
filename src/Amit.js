8a7030fbc9a9c0818a5fdf27a2fe905a6ddaee53
Amit Kumar singh <amitsingh.ug19.ee@nitp.ac.in>
2022-11-16 19:48:55 +0530

    to clear the log

---
+++ src/Amit.js
@@ -0,0 +1,11 @@
+import React,{useEffect} from "react";
+import './amit.css'
+const Amit=()=>{
+  return (
+    <div className="amit2">
+    <h3 className="amit">Amit</h3>
+    </div>
+  );
+}
+
+export default Amit;
